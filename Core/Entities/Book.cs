cf38314f6763c37f9b36c786333d27ed7781bf79
Michael <grishchenko.m@yandex.ru>
2016-10-12 18:26:40 +0300

    Create Core proj. (DAL) Add new entities.

---
+++ Core/Entities/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entities
+{
+    public class Book : EntityBase
+    {
+        public int AuthorId { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ Core/Entities/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entities
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
