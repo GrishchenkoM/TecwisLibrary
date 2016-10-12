ce53c3721925f28a95b610f332686b3c51583d5e
Michael <grishchenko.m@yandex.ru>
2016-10-12 18:42:32 +0300

    Create DbDataContext. Add EF reference.

---
+++ BusinessLogic/DbDataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using Core.Entities;
+
+namespace BusinessLogic
+{
+    public class DbDataContext : DbContext
+    {
+        public DbDataContext() : base("DefaultConnection")
+        { }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
