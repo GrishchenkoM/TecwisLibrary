457d34d15ca41abc0e7b27f4eb57b701d48d8cb9
Michael <grishchenko.m@yandex.ru>
2016-10-12 18:33:54 +0300

    Create BusinessLogic proj. Add IRepository interface.

---
+++ BusinessLogic/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Core.Entities;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface IRepository<T> where T : EntityBase
+    {
+        List<T> Get();
+        T Create(T obj);
+        T Update(T obj);
+        int Delete(T obj);
+    }
+}
