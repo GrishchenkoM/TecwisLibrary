8b52770bf25a81164e8fe6b3b0b7460dcf85f815
Michael <grishchenko.m@yandex.ru>
2016-10-13 12:59:37 +0300

    Create Web proj. Add Autofac reference. Add DI realization with Autofac.

---
+++ Web/IoC/AutofacConfig.cs
@@ -0,0 +1,46 @@
+﻿using System.Configuration;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using BusinessLogic;
+using BusinessLogic.Repositories;
+using Core.Entities;
+
+namespace Web.IoC
+{
+    public class AutofacConfig
+    {
+        public static void ConfigureContainer()
+        {
+            // получаем экземпляр контейнера
+            var builder = new ContainerBuilder();
+
+            // регистрируем контроллер в текущей сборке
+            builder.RegisterControllers(typeof(WebApiApplication).Assembly);
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // регистрируем споставление типов
+            builder.RegisterType<DbDataContext>().As<DbDataContext>().WithParameter("DefaultConnection",
+                                                   ConfigurationManager.ConnectionStrings[1].ConnectionString);
+
+            builder.RegisterType<AuthorRepository>().As<Repository<Author>>();
+            builder.RegisterType<BookRepository>().As<Repository<Book>>();
+            
+
+            builder.RegisterType<DataManager>().As<IDataManager>();
+
+            // создаем новый контейнер с теми зависимостями, которые определены выше
+            var container = builder.Build();
+
+            // установка сопоставителя зависимостей
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            var apiResolver = new AutofacWebApiDependencyResolver(container);
+            GlobalConfiguration.Configuration.DependencyResolver = apiResolver;
+        }
+    }
+}
