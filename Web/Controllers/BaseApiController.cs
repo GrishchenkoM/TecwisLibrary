5651c242b09d0045ebc8cf806d3cb45783eeb8ef
Michael <grishchenko.m@yandex.ru>
2016-10-13 13:27:11 +0300

    Create BaseApiController

---
+++ Web/Controllers/BaseApiController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using BusinessLogic;
+using Web.Models.EntityModels.Interfaces;
+
+namespace Web.Controllers
+{
+    public class BaseApiController<TModelFactory> 
+        : ApiController where TModelFactory : IModelFactory, new()
+    {
+        public BaseApiController(IDataManager dataManager)
+        {
+            DataManager = dataManager;
+            ModelFactory = new TModelFactory();
+        }
+
+        public IDataManager DataManager { get; }
+        public TModelFactory ModelFactory { get; }
+    }
+}
